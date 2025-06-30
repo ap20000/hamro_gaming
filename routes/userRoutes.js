54c05032a2cd9163581f17eb97edcbe31b8d5957
Prayatna-Rajkarnikar <prayatna.rajkarnikar@gmail.com>
2025-06-30 21:03:27 +0545

    small change

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,21 +1,35 @@
 // routes/productRoutes.js
-import express from 'express';
-import { getAllGamingProducts, getGamingProductById } from '../controllers/userController.js';
-import { addToCart, getCart, removeFromCart } from '../controllers/cartController.js';
-import { placeOrder, getMyOrders } from '../controllers/orderController.js';
-import { protect } from '../middlewares/authMiddleware.js';
+import express from "express";
+import {
+  getAllGamingProducts,
+  getGamingProductById,
+} from "../controllers/userController.js";
+import {
+  addToCart,
+  getCart,
+  removeFromCart,
+} from "../controllers/cartController.js";
+import {
+  placeOrder,
+  getMyOrders,
+  getOrderById,
+  claimGiftcardKey,
+} from "../controllers/orderController.js";
+
+import { protect } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
-router.get('/products', getAllGamingProducts); // GET /api/products
-router.get('/products/:id', getGamingProductById); 
+router.get("/products", getAllGamingProducts);
+router.get("/products/:id", getGamingProductById);
 
 // Cart Routes
-router.post('/cart', protect, addToCart);
-router.get('/cart', protect, getCart);
-router.delete('/cart/:productId', protect, removeFromCart);
+router.post("/cart", protect, addToCart);
+router.get("/cart", protect, getCart);
+router.delete("/cart/:productId", protect, removeFromCart);
 
-// Order Routes
-router.post('/orders', protect, placeOrder);
-router.get('/orders/my', protect, getMyOrders);
+router.post("/orders", protect, placeOrder);
+router.get("/orders/my", protect, getMyOrders);
+router.get("/orders/my/:id", protect, getOrderById);
+router.post("/orders/:id/claim", protect, claimGiftcardKey);
 
 export default router;
