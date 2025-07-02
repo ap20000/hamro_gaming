eb19a5365b36835a36c09e23f208b3d6f9ef136d
Prayatna-Rajkarnikar <prayatna.rajkarnikar@gmail.com>
2025-07-02 11:00:18 +0545

    feat: chat

--- routes/livechat/conversationRoutes.js
+++ routes/livechat/conversationRoutes.js
@@ -1,9 +1,19 @@
-import express from 'express';
-import { getAllConversations, createConversation } from '../../controllers/conversationController.js';
+import express from "express";
+import {
+  getAllConversations,
+  createConversation,
+  getConversationById,
+} from "../../controllers/conversationController.js";
 
 const router = express.Router();
 
-router.get('/', getAllConversations);
-router.post('/', createConversation);
+// GET /api/conversations - Get all conversations
+router.get("/", getAllConversations);
+
+// POST /api/conversations - Create a new conversation
+router.post("/", createConversation);
+
+// GET /api/conversations/:id - Get a specific conversation
+router.get("/:id", getConversationById);
 
 export default router;
--- routes/livechat/messageRoutes.js
+++ routes/livechat/messageRoutes.js
@@ -1,8 +1,15 @@
-import express from 'express';
-import { getMessagesByConversation } from '../../controllers/messageController.js';
+import express from "express";
+import {
+  getMessagesByConversation,
+  createMessage,
+} from "../../controllers/messageController.js";
 
 const router = express.Router();
 
-router.get('/:id', getMessagesByConversation);
+// GET /api/messages/:id - Get messages by conversation ID
+router.get("/:id", getMessagesByConversation);
+
+// POST /api/messages - Create a new message
+router.post("/", createMessage);
 
 export default router;
