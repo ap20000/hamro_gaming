e21b7035634d5b1e649acb06cf7d44d8e565f086
Abhishek_05 <108204294+ap20000@users.noreply.github.com>
2025-07-02 10:43:50 +0545

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -21,7 +21,6 @@ import { protect } from "../middlewares/authMiddleware.js";
 
 
 
-import { protect } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.get("/products", getAllGamingProducts); // GET /api/products
